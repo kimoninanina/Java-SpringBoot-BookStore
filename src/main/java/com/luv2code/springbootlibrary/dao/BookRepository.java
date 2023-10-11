3bcab084b99f08c66a76e1c92584d0c4422d8e13
kimoninanina <nina02929@gmail.com>
2023-10-11 12:59:07 -0700

    SpringBoot add BookCategory

--- src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
@@ -1,8 +1,15 @@
 package com.luv2code.springbootlibrary.dao;
 
 import com.luv2code.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 
 }
