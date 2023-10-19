a01dbea3b832f16d163f0cea1d3466dfedc5cb51
kimoninanina <nina02929@gmail.com>
2023-10-19 14:04:11 -0700

    SpringBoot Add messages endpoint

---
+++ src/main/java/com/luv2code/springbootlibrary/service/MessagesService.java
@@ -0,0 +1,29 @@
+package com.luv2code.springbootlibrary.service;
+
+import com.luv2code.springbootlibrary.dao.MessageRepository;
+import com.luv2code.springbootlibrary.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class MessagesService {
+
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessagesService(MessageRepository messageRepository) {
+
+        this.messageRepository = messageRepository;
+    }
+
+    public void postMessage(Message messageRequest, String userEmail) {
+        Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
+        message.setUserEmail(userEmail);
+        messageRepository.save(message);
+    }
+
+}
