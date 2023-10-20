2b91961a859a10bd1e2f2af64578c7a526f53092
kimoninanina <nina02929@gmail.com>
2023-10-20 16:55:07 -0700

    SpringBoot Add message admin

--- src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
@@ -2,6 +2,7 @@ package com.luv2code.springbootlibrary.controller;
 
 import com.luv2code.springbootlibrary.entity.Message;
 
+import com.luv2code.springbootlibrary.requestmodels.AdminQuestionRequest;
 import com.luv2code.springbootlibrary.service.MessagesService;
 import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,16 +27,16 @@ public class MessagesController {
         messagesService.postMessage(messageRequest, userEmail);
     }
 
-//    @PutMapping("/secure/admin/message")
-//    public void putMessage(@RequestHeader(value="Authorization") String token,
-//                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
-//        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
-//        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
-//        if (admin == null || !admin.equals("admin")) {
-//            throw new Exception("Administration page only.");
-//        }
-//        messagesService.putMessage(adminQuestionRequest, userEmail);
-//    }
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value="Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        messagesService.putMessage(adminQuestionRequest, userEmail);
+    }
 
 }
 
